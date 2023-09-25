ab88a281099f1a85571cf15e387aa0cd5dad72cc
hyunseong hwang <ree7243@gmail.com>
2023-09-25 12:13:37 +0900

    add shopping mall

--- 6-shopping-mall/src/api/requests.js
+++
@@ -1,9 +0,0 @@
-const requests = {
-  fetchAllProducts: 'products',
-  fetchJewelery: 'products/category/jewelery',
-  fetchMen: "products/category/men's clothing",
-  fetchElectronics: 'products/category/electronics',
-  fetchWomen: "products/category/women's clothing",
-}
-
-export default requests;
