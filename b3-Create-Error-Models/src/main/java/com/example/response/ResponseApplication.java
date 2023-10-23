9874ed3adeba5af890e8ae9e9cbf88c785aeb694
hstla <ree7243@gmail.com>
2023-10-23 13:05:08 +0900

    Revert "Revert "3번 과제 시작""
    This reverts commit 4969eb9b1d27757adcc381d3d64bda614e7b0cf8.

---
+++ b3-Create-Error-Models/src/main/java/com/example/response/ResponseApplication.java
@@ -0,0 +1,12 @@
+package com.example.response;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResponseApplication.class, args);
+	}
+}
